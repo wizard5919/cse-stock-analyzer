449e7d9e41efd612d112fea938f98a541fa93c10
youssef sbai <155957681+wizard5919@users.noreply.github.com>
2025-08-18 19:46:49 +0100

    Add files via upload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/globals.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/globals.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
